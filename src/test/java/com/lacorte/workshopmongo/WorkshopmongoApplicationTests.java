38da9ed0deea3d65d5db5aae411ec2b3c1c2c660
dlacorte10 <daniel.lacorte10@gmail.com>
2026-05-12 14:27:59 -0300

    Project Created

---
+++ src/test/java/com/lacorte/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacorte.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

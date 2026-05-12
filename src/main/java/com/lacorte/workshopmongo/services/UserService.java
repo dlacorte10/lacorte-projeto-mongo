26b27da642f2d0cfc9d7dc279546e5d871cd25ac
dlacorte10 <daniel.lacorte10@gmail.com>
2026-05-12 17:21:49 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/lacorte/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.lacorte.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lacorte.workshopmongo.domain.User;
+import com.lacorte.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}

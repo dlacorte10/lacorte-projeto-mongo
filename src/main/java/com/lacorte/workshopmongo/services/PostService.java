703124043d6053bbbf433bbc90dad37422d92cb7
dlacorte10 <daniel.lacorte10@gmail.com>
2026-05-13 14:14:36 -0300

    Simple query using query methods

--- src/main/java/com/lacorte/workshopmongo/services/PostService.java
+++ src/main/java/com/lacorte/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.lacorte.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lacorte.workshopmongo.domain.Post;
-import com.lacorte.workshopmongo.domain.User;
 import com.lacorte.workshopmongo.repository.PostRepository;
 import com.lacorte.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -19,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
